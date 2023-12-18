03f4c2bf8913529fca893c5b3439af77b652c33c
augusto <augustoantonelli88@gmail.com>
2023-12-18 01:46:59 -0300

    agrego el link para volver a index

--- src/Components/Generals/Layout/NavBar/NavBar.jsx
+++ src/Components/Generals/Layout/NavBar/NavBar.jsx
@@ -1,13 +1,17 @@
 import './NabBar.css';
 import logoclaro from './img/logo claro.svg'
 
-export default function Navbar({handleOpen}) {
+export default function Navbar({ handleOpen }) {
   return (
     <div className="navbar">
+
+      <a href="/">
         <img src={logoclaro} alt="" />
-        <div className='links'>
-            <button onClick={handleOpen}> Join Us </button>
-        </div>
+      </a>
+
+      <div className='links'>
+        <button onClick={handleOpen}> Join Us </button>
+      </div>
     </div>
   );
 }
